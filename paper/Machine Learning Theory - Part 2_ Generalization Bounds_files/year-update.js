b5eeebd02ada8416a285bf6059cc6d0e126fae11
Matt17BR <matteo.mazzarelli@gmail.com>
2024-11-18 17:10:37 +0100

    Remove cached files from git

--- paper/Machine Learning Theory - Part 2_ Generalization Bounds_files/year-update.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("SHOUT!!!!");
-    var dateobj = new Date();
-    var currentYear = dateobj.getFullYear();
-
-    document.querySelector("#ccyear").innerHTML = currentYear;
-});
